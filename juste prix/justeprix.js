b006b4287672a9aa527b89f95345a6bd12d38e98
Mickael De Jésus <djizou@hotmail.fr>
2023-02-10 03:11:31 +0100

    création jeu du pendu

--- juste prix/justeprix.js
+++ juste prix/justeprix.js
@@ -2,45 +2,20 @@
 // L'utilisateur fera des propositions
 // Contniuer tant qu'il n'a pas la bonne proposition
 
-let numberToFind = 0;
-const resultDiv = document.getElementById('resultDiv');
-const timer     = document.getElementById('timer');
-let tempsRestant = 0;
+import { Confetti } from "../lib/confetti.js";
+
+let numberToFind     = 0;
+const resultDiv      = document.getElementById('resultDiv');
+const timer          = document.getElementById('timer');
+const GamePropalDiv  = document.getElementById('GamePropalDiv');
+let tempsRestant     = 0;
 let compteurInterval = null;
 
 
 document.getElementById('beginGame').addEventListener('click', function() {
     // Lancer la partie
     // Récupérer un chiffre aléatoire
-    numberToFind = getRandomInt(1000);
-    tempsRestant = 30;
-    if(compteurInterval != null) {
-        clearInterval(compteurInterval);
-    }
-        compteurInterval = setInterval(() => {
-            timer.innerText = tempsRestant + ' seconde(s) ';
-            tempsRestant--;
-            if(tempsRestant >= 20) {
-                timer.classList.remove('warning');
-                timer.classList.remove('danger');
-                timer.classList.add('cool');
-            }
-            else if(tempsRestant >= 10) {
-                timer.classList.remove('cool')
-                timer.classList.remove('danger')
-                timer.classList.add('warning')
-            }
-            else if(tempsRestant < 10) {
-                timer.classList.remove('cool');
-                timer.classList.remove('warning');
-                timer.classList.add('danger');
-            }
-            else if(tempsRestant < 0) {
-                clearInterval(compteurInterval);
-                // Partie terminée
-            }
-            
-        }, 1000);
+    launchGame();
 });
 
 document.getElementById('ckeckPropalButton').addEventListener('click', function() {
@@ -67,6 +42,54 @@ function checkPropal() {
     } 
     else if(numberToFind == numberPropal) {
         resultDiv.innerHTML = "C'est gagné !"
+        endGame(true);
     }
 };
+
+function launchGame() {
+    numberToFind = getRandomInt(1000);
+    console.log(numberToFind);
+    tempsRestant = 30;
+    GamePropalDiv.style.display = 'block';
+    if(compteurInterval != null) {
+        clearInterval(compteurInterval);
+    }
+        compteurInterval = setInterval(() => {
+            timer.innerText = tempsRestant;
+            tempsRestant--;
+            if(tempsRestant >= 20) {
+                timer.classList.remove('warning');
+                timer.classList.remove('danger');
+                timer.classList.add('cool');
+            }
+            else if(tempsRestant > 10) {
+                timer.classList.remove('cool')
+                timer.classList.remove('danger')
+                timer.classList.add('warning')
+            }
+            else if(tempsRestant >= 0) {
+                timer.classList.remove('cool');
+                timer.classList.remove('warning');
+                timer.classList.add('danger');
+            }
+            else if(tempsRestant < 0) {
+                clearInterval(compteurInterval);
+                endGame(false);
+            }   
+        }, 1000);
+}
+
+function endGame(gagne) {
+    if(gagne) {
+        Confetti.launchAnimationConfetti();
+        setTimeout(() => {
+            Confetti.stopAnimationConfetti();
+        }, 5000);
+    } else {
+        alert("T'es mauvais Jack, c'est la piquette Jack !! Tu sais pas jouer Jack !");
+    }
+    GamePropalDiv.style.display = 'none';
+    clearInterval(compteurInterval);
+    
+}
 
